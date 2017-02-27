10d3b515d2af76e631c58efae64df54a34ef3fbf
Minified <gora.pub@gmail.com>
2017-02-27 22:44:59 +0100

    travis

--- tests/SierotkiTest.js
+++ tests/SierotkiTest.js
@@ -1,6 +1,8 @@
 var assert = require('assert');
 var Sierotki = require('./../Sierotki.js').Sierotki;
 
+console.log(process.env.PWD,process.cwd())
+
 let string_1 = 'Zwykla sierota a sd :D sd<p ds> <a href="dsa dasd  s s">Zwykla sierota a sd :D sd</a><p>S sd</p><p>s sd</p>koniec ';
 
 describe('Sierotki', () => {
